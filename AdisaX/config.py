0904d58c77587fac63eed086a69090cd8dab8c81
Kairnex <codexkairnex@gmail.com>
2025-05-08 23:23:32 +0530

    Update config.py

--- AdisaX/config.py
+++ AdisaX/config.py
@@ -4,21 +4,21 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "" # integer value, dont use ""
-    API_HASH = ""
-    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 5465943450 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    API_ID = "21189715" # integer value, dont use ""
+    API_HASH = "988a9111105fd2f0c5e21c2c2449edfd"
+    TOKEN = "8006165946:AAFoIk1txo28CGOg1ekOrGuEyG-VkIfRj6c"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 6999372290 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "alonegroup121"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "pahadichat"  # Your own group for support, do not add the @
     START_IMG = "https://te.legra.ph/file/6f83d17f269fddb45a217.jpg"
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= ""
+    MONGO_DB_URI= "mongodb+srv://codexkairnex:gm6xSxXfRkusMIug@cluster0.bplk1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     # RECOMMENDED
     DATABASE_URL = "postgres://caxwiincbnmrgi:39af849ee745178987393bce5eb4752f6e3c37c42213002f4865d5293c173421@ec2-54-225-254-115.compute-1.amazonaws.com:5432/d32c6c3sqabr9f"  # A sql database url from elephantsql.com
     CASH_API_KEY = (
-        ""  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "8SOSKTRI0KPL"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = ""
+    TIME_API_KEY = "https://timezonedb.com/api"
     # Get your API key from https://timezonedb.com/api
 
     # Optional fields
